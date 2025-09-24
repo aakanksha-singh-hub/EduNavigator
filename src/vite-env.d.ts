14edcc95de223515fc73c5708d2d4d2bd36fa45e
Aakanksha Singh <aakanksha.singh0205@gmail.com>
2025-09-24 06:30:20 +0530

    setting up the ui and adding components for it

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+interface ImportMeta {
+  env: {
+    VITE_SERVER_URL: string
+    VITE_CLIENT_URL: string
+    VITE_GEMINI_API_KEY: string
+    PROD: boolean
+    DEV: boolean
+    MODE: string
+  }
+}
